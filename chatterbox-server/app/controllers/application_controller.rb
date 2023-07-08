b72d71c5498d237f9373c1e75921c8f524070886
Michael Elfassy <mike.elfassy@gmail.com>
2023-07-08 19:39:01 -0400

    commit tree f0c585dfc632bc4e5fab963fed20699430fe87f2

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,35 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
-  
+  get '/messages' do
+    # see the BakedGood class for the  method definition of `.by_price`
+    messages = Message.all.order(created_at: :asc)
+    messages.to_json
+  end
+
+  post '/messages' do
+    message = Message.create(
+      body: params[:body],
+      username: params[:username],
+      # created_at: Time.now
+      # updated_at: Time.now
+    )
+    message.to_json
+  end
+
+  patch '/messages/:id' do
+    message = Message.find(params[:id])
+    message.update(
+      body: params[:body]
+      # updated_at: Time.now
+    )
+    message.to_json
+  end
+
+  delete '/messages/:id' do
+    message = Message.find(params[:id])
+    message.destroy
+    message.to_json
+  end
+
 end
